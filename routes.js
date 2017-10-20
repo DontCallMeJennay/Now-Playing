b996ee9064486dd8dac2b1d93183452bbaef86bc
Jenni <silverrain64@gmail.com>
2017-10-20 02:58:37 -0700

    The whole thing works *some* of the time.

--- routes.js
+++ routes.js
@@ -17,6 +17,7 @@ router.get("/", function(request, response) {
 
 router.get('/streams', function(req, res, next) {
     //let user = req.params.user;
+    T_DATA = [];
     var follows = new Promise((resolve, reject) => {
         let options = {
             uri: "https://api.twitch.tv/kraken/users/silverrain64/follows/channels?limit=5&sortby=last_broadcast",
@@ -28,37 +29,35 @@ router.get('/streams', function(req, res, next) {
             resolve(body);
         });
     });
-    
+
     Promise.resolve(follows).then((data) => {
-        res.send(data);
+        var whosLive = new Promise((resolve, reject) => {
+            for (var i = 0; i < 3; i++) {
+                let name = data.follows[i].channel.name;
+                let x = makePromise(name);
+                T_DATA.push(x);
+            }
+        });
+
+        Promise.all(T_DATA).then((data) => {
+            res.send(data);
+        });
+
+
     });
 });
 
-function seeWhosLive(data) {
-    let len = data._total;
-    var liveList = new Promise((resolve, reject) => {
-        for (var i = 0; i < 3; i++) {
-            let x = new Promise((resolve, reject) => {
-                console.log('loop promise made: ', i);
-                let name = data.follows[i].channel.name;
-                let options = {
-                    uri: "https://api.twitch.tv/kraken/streams/" + name,
-                    headers: { 'Client-ID': KEY_T },
-                    json: true
-                }
-                rp(options, function(err, res, body) {
-                    if (err) next(err);
-                    body.stream ? live.push(body.stream.channel) : live.push(`user ${name} is offline`);
-                });
-            });
+function makePromise(name) {
+    return new Promise((resolve, reject) => {
+        let options = {
+            uri: "https://api.twitch.tv/kraken/streams/" + name,
+            headers: { 'Client-ID': KEY_T },
+            json: true
         }
-        console.log('loop finished');
-        resolve(liveList);
-    });
-    var reply = Promise.resolve(liveList);
-    console.log("Reply: ", reply)
-    reply.then(() => {
-        console.log('Second promise: ', liveList);
+        rp(options, function(err, res, body) {
+            if (err) next(err);
+            body.stream ? resolve(body.stream) : resolve(`user ${name} is offline`);
+        });
     });
 }
 
