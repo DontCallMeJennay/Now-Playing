97cd7ca6d1e5f22983a0c91e0c55e8ed9fd0e288
Jenni <silverrain64@gmail.com>
2018-01-04 22:00:41 -0800

    Why can't child components see the VueX store...?

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -1,80 +1,3 @@
-const steamModule = {
-    namespaced: true,
-    state: {
-        active: true,
-        results: [],
-        signedIn: false,
-        username: "76561198010153724"
-    },
-    mutations: {
-        changeName(state, str) {
-            this.state.s.username = str
-        },
-        makeActive(state, bool) {
-            this.state.s.active = bool
-        },
-        signedIn(state, bool) {
-            this.state.s.signedIn = bool
-        },
-        update(state, payload) {
-            this.state.s.results = payload
-        }
-    }
-}
-
-const twitchModule = {
-    namespaced: true,
-    state: {
-        active: true,
-        results: [],
-        signedIn: false,
-        username: "silverrain64"
-    },
-    mutations: {
-        changeName(state, str) {
-            this.state.t.username = str
-        },
-        makeActive(state, bool) {
-            this.state.t.active = bool
-        },
-        signedIn(state, bool) {
-            this.state.t.signedIn = bool
-        },
-        update(state, payload) {
-            this.state.t.results = payload
-        }
-    }
-}
-
-const youtubeModule = {
-    namespaced: true,
-    state: {
-        active: true,
-        results: [],
-        signedIn: false
-    },
-    mutations: {
-        makeActive(state, bool) {
-            this.state.y.active = bool
-        },
-        signedIn(state, bool) {
-            this.state.y.signedIn = bool
-        },
-        update(state, payload) {
-            this.state.y.results = payload
-        }
-    }
-}
-
-var Ledger = new Vuex.Store({
-    namespaced: true,
-    modules: {
-        s: steamModule,
-        t: twitchModule,
-        y: youtubeModule
-    }
-})
-
 var vm = new Vue({
     el: "#vue-app",
     store: Ledger,
@@ -140,5 +63,23 @@ var vm = new Vue({
             this.setUser(x, "twitchName");
             this.getStreamList(this.twitchName);
         }
-    }
+    },
+    template: `
+        <div>
+        <section class="twitch" id="tList">
+            <twitch-list content-title="Twitch.tv" :content-data="twitchResults" :get-stream-list="getStreamList">
+            </twitch-list>
+        </section>
+        <section id="yList">
+            <youtube-list content-title="YouTube" :content-data="ytResults">
+            </youtube-list>
+        </section>
+        <!--
+        <section id="sList">
+            <steam-list content-title="Steam" :content-data="steamResults">
+            </steam-list>
+        </section>
+    -->
+        </div>
+    `
 });
--- src/scripts/vue-components.js
+++ src/scripts/vue-components.js
@@ -1,3 +1,26 @@
+Vue.component("control-panel", {
+    props: {
+        getName: Function,
+        user: String
+    },
+    data: function() {
+        return {
+            signedIn: false
+        }
+    },    
+    template: `
+        <section>
+            <div>
+                <label for="username">Enter Twitch.tv username</label>
+                <input type="text" v-model=user id="username"/>
+                <button class="btn-filter" id="twitch-auth" @click=getName()>Get follow list</button></button>
+            </div>
+            <div>
+                <button class="btn-filter" id="authorize-button" style="display: block;">Authorize Y</button>
+                <button class="btn-filter" id="signout-button" style="display: block;">Sign out of YouTube</button>
+            </div>
+        </section>`
+})
 
 Vue.component("steam-list", {
     props: {
