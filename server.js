bf64b2dc2589245b9ac7cec92ad2b0632f6b9cea
Jenni <silverrain64@gmail.com>
2018-01-05 21:03:05 -0800

    trying to deploy...

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var index = require('public/routes');
+var index = require('/public/routes');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 var PORT = process.env.PORT || 3000;
 
-app.use('/', express.static('./public'));
+app.use('/', express.static('public/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
